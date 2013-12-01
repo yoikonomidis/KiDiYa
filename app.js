520d52f5c92c0e8165c9c8fcbf2b52f446494874
dmamalis <dimitris.mamalis@gmail.com>
2013-12-01 19:13:46 +0100

    Express based app + Android post

---
+++ app.js
@@ -0,0 +1,52 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+//Connect to MongoDB
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/helloworld', routes.helloworld);
+app.get('/emplist', routes.emplist(db));
+app.get('/newemp', routes.newemp);
+app.get('/delemp', routes.delemp);
+
+app.post('/addemp',routes.addemp(db));
+app.post('/rememp',routes.rememp(db));
+app.post('/androiddata',routes.androiddata);
+//TODO write androiddata to the database
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,96 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.helloworld = function(req, res){
+	res.render('helloworld', {title: 'HelloWorld'});
+};
+
+exports.emplist = function(db){
+	return function(req, res){
+		var collection = db.get('empcollection');
+		collection.find({},{},function(e,docs){
+			res.render('emplist',{
+				"emplist":docs
+			});
+		});
+	};
+};
+
+exports.newemp = function(req,res){
+	res.render('newemp', { title: 'Add Employee'});
+};
+
+exports.addemp = function(db){
+	return function(req,res){
+		console.log("DEBUG")
+		//Get form values
+		var firstName = req.body.firstname;
+		var lastName  = req.body.lastname;
+		var managerId = req.body.managerid;
+
+		//Set collection
+		var collection = db.get('empcollection');
+
+		//Submit to the DB
+		collection.insert({
+				"firstName": firstName,
+				"lastName" : lastName,
+				"managerId": managerId
+		}, function(err,doc){
+			if(err){
+				//if it failed, return error
+				res.send("There was a problem adding the information to the database.");
+			}
+			else{
+				//Forward to success page
+				res.redirect("emplist");
+				//And set the header so the address bar doesn't still say /addemp
+				res.location("emplist");
+			}
+		});
+	}
+}
+
+exports.delemp = function(req,res){
+	res.render('delemp', { title: 'Remove Employee'});
+};
+
+exports.rememp = function(db){
+	return function(req,res){
+		//Get form values
+		var firstName  = req.body.firstname;
+		console.log(firstName);
+
+		//Set collection
+		var collection = db.get('empcollection');
+
+		//Submit to the DB
+		collection.remove({
+				"firstName" : firstName
+		 }, function(err,doc){
+			if(err){
+				//if it failed, return error
+				res.send("There was a problem removing the information from the database.");
+			}
+			else{
+				console.log(firstName + " removed");
+				//Forward to success page
+				res.redirect("emplist");
+				//And set the header so the address bar doesn't still say /addemp
+				res.location("emplist");
+			}
+		});
+	}
+}
+
+exports.androiddata = function(req, res){
+
+	var data = req.body.firstName;
+	console.log(data);
+}
